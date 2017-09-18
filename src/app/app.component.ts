a8b7dcb2466ee38afec3e2814c13c9245f7fbf33
Svb home <sergio.valverde.b@gmail.com>
2017-09-17 20:18:15 -0600

    Adding temporary navigation, will eventually replace with actual router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,4 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent {
+  activePage:string = "recipes";
+  
+  onPageChange(data){
+    this.activePage = data.active;
+  }
+}
