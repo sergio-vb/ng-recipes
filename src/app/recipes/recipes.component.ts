8cf575b9a622beb6ad194cc8194f26617f1f5e8f
Svb home <sergio.valverde.b@gmail.com>
2017-10-29 12:22:07 -0600

    Refactoring functionality of selecting a recipe to use a service

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -12,11 +12,11 @@ import { RecipeService } from './recipe.service';
 export class RecipesComponent implements OnInit {
   selectedRecipe: Recipe;
 
-  constructor() {}
+  constructor(private recipeService: RecipeService) {}
 
-  ngOnInit() {}
-
-  onRecipeSelected(recipe: Recipe) {
-    this.selectedRecipe = recipe;
+  ngOnInit() {
+    this.recipeService.recipeSelected.subscribe(recipe => {
+      this.selectedRecipe = recipe;
+    });
   }
 }
