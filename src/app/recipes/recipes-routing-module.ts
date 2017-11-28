e6277b1417307a777f11be06e8974c9f2e8e2ef2
Sergio Valverde <sergio@its-Mac-mini.local>
2017-11-28 09:52:08 -0600

    Moving recipes routes into a separate routing module

---
+++ src/app/recipes/recipes-routing-module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { RecipesComponent } from './recipes.component';
+import { RecipeDefaultComponent } from './recipe-default/recipe-default.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { AuthGuard } from '../auth/auth-guard.service';
+
+const routes: Routes = [
+    { path: 'recipes', component: RecipesComponent, children: [
+        { path: '', component: RecipeDefaultComponent},
+        { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard]},
+        { path: ':id', component: RecipeDetailComponent},
+        { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard]}
+    ]}
+];
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class RecipesRoutingModule {}
--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { RecipeListComponent } from './recipe-list/recipe-list.component';
 import { RecipeItemComponent } from './recipe-item/recipe-item.component';
@@ -7,7 +8,7 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipesComponent } from './recipes.component';
 import { RecipeDefaultComponent } from './recipe-default/recipe-default.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
-
+import { RecipesRoutingModule } from './recipes-routing-module';
 
 @NgModule({
     declarations: [
@@ -19,7 +20,9 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
         RecipeEditComponent,
     ],
     imports: [
-        CommonModule /* common directives, ngClass, ngFor, ngIf */
+        CommonModule, /* common directives, ngClass, ngFor, ngIf */
+        ReactiveFormsModule,
+        RecipesRoutingModule
     ]
 })
 export class RecipesModule{
