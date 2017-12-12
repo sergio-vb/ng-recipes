4d3081814655c61c7d1cfda77bb19bddeea823a2
Svb home <sergio.valverde.b@gmail.com>
2017-12-11 22:34:04 -0600

    Changing how the recipe description is displayed, should support html formatting

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -37,7 +37,9 @@ export class RecipeService{
     
     new Recipe(
       'Skillet Chicken and Quinoa with Fresh Salsa',
-      'A filling and fresh dish. Topping with the homemade salsa is a must!',
+      `<p>A filling and fresh dish. Topping with the homemade salsa is a must!</p><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptatibus ipsum debitis provident animi recusandae molestiae natus iure aspernatur, voluptate, explicabo nemo! Dignissimos laboriosam dolor accusantium suscipit excepturi commodi non?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quaerat odio possimus, nemo, natus commodi asperiores quam molestias architecto, est animi vero libero. Quia voluptatibus incidunt tempora cum sit distinctio.Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quaerat odio possimus, nemo, natus commodi asperiores quam molestias architecto, est animi vero libero. Quia voluptatibus incidunt tempora cum sit distinctio.</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quaerat odio possimus, nemo, natus commodi asperiores quam molestias architecto, est animi vero libero. Quia voluptatibus incidunt tempora cum sit distinctio.Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quaerat odio possimus, nemo, natus commodi asperiores quam molestias architecto, est animi vero libero. Quia voluptatibus incidunt tempora cum sit distinctio.</p>`,
       'http://images.media-allrecipes.com/userphotos/720x405/4535588.jpg',
       [new Ingredient('Chicken', 200), new Ingredient('Tomato', 4)]
     ),
