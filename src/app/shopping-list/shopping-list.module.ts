7e6105f4c271a1785cf10c4a4c049e909ac5e290
Svb home <sergio.valverde.b@gmail.com>
2018-01-01 15:24:47 -0600

    General improvements to copy, for homepage, recipe list, and shopping list

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { ShoppingListComponent } from './shopping-list.component';
 import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-edit.component';
@@ -13,7 +14,8 @@ import { ShoppingListEditComponent } from './shopping-list-edit/shopping-list-ed
     imports:[
         CommonModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        RouterModule
     ]
 })
 export class ShoppingListModule{}
