46c507ce61762bf1a6200efc7f618a303815d67d
Sergio Valverde <sergio@its-mac-mini.avantica.avanticatec.net avanticape.avantica.avanticatec.net avantek-sc.avantica.avanticatec.net avanticabol.avantica.avanticatec.net avantica.net>
2018-02-14 17:10:58 -0600

    Improving password validation for signup page

--- src/app/auth/password-confirmation.directive.ts
+++ src/app/auth/password-confirmation.directive.ts
@@ -1,23 +1,32 @@
 import { Directive, Input, OnChanges, SimpleChanges } from '@angular/core';
-import { AbstractControl, NG_VALIDATORS, Validator, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, NG_VALIDATORS, Validator } from '@angular/forms';
 
 //The validator function can be used by itself in reactive forms. Use the directive for template-driven forms
-export function passwordConfirmationValidator(equalTo: string): ValidatorFn {
-    return (control: AbstractControl): {[key: string]: any} => {
-        //If the values of the two controls are different, return an error object
-        return (control.value !== control.root.get(equalTo).value) ? {'passwordConfirmation': true} : null;
-    };
-}
+export function passwordConfirmationValidator(control: AbstractControl): {[key: string]: any} {
+    //If the values of the two controls are different, return an error object
+    return (control.value.password !== control.value['password-confirmation']) ? {'passwordConfirmation': true} : null;
+};
+
 
 @Directive({
     selector: '[appPasswordConfirmation]',
     providers: [{provide: NG_VALIDATORS, useExisting: PasswordConfirmationValidatorDirective, multi: true}]
 })
 export class PasswordConfirmationValidatorDirective implements Validator {
-    @Input('appPasswordConfirmation') passwordConfirmation: string;
-
     validate(control: AbstractControl): {[key: string]: any} {
-        return this.passwordConfirmation ? passwordConfirmationValidator(this.passwordConfirmation)(control) : null;
+        return passwordConfirmationValidator(control);
     }
 }
 
+
+
+
+
+
+// Alternative implementation: If any parameters are needed, use a function that will return the validator function:
+// export function passwordConfirmationValidator(equalTo: string): ValidatorFn {
+//     return (control: AbstractControl): {[key: string]: any} => {
+//         //If the values of the two controls are different, return an error object
+//         return (control.value !== control.root.get(equalTo).value) ? {'passwordConfirmation': true} : null;
+//     };
+// }
