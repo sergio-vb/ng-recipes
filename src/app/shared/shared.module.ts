dde6f5e53dcb74b528ff6e9f66afd0bdac1f9d1b
Svb home <sergio.valverde.b@gmail.com>
2018-02-18 20:09:40 -0600

    Added a reusable loader component, to display a loading animation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,24 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { DropdownDirective } from './dropdown.directive';
-import { ObjectKeys } from './object-keys.pipe';
-import { ConfirmationModalComponent } from './confirmation-modal/confirmation-modal.component';
 import { CanDeactivateGuard } from './can-deactivate-guard.service';
+import { ConfirmationModalComponent } from './confirmation-modal/confirmation-modal.component';
+import { DropdownDirective } from './dropdown.directive';
+import { LoaderComponent } from './loader/loader.component';
+import { ObjectKeys } from './object-keys.pipe';
 
 @NgModule({
     declarations: [
+        ConfirmationModalComponent,
         DropdownDirective, 
-        ObjectKeys,
-        ConfirmationModalComponent
+        LoaderComponent,
+        ObjectKeys
     ],
     imports: [
         CommonModule
     ],
     exports: [
         CommonModule, /* common directives, ngClass, ngFor, ngIf */
-        DropdownDirective,
         ConfirmationModalComponent,
+        DropdownDirective,
+        LoaderComponent,
         ObjectKeys
     ]
 })
